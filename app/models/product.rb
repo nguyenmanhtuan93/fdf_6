c6eed50b4af117909b1eeb8ff1b0128016a59bb4
VuThiHien94 <khoanglang2368@gmail.com>
2016-07-04 15:31:46 +0700

    show info product

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,10 @@ class Product < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   enum classify: ["food", "drink"]
+
+  validates :category_id, presence: true
+  validates :rating, format: {with: /\A\d+(?:.\d{0,2})?\z/},
+    numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
+
+  mount_uploader :image, ImageUploader
 end
